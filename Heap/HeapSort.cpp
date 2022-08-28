3d6a465a96dcea0551957bec35b6076df1131db6
Yash Rajeshbhai Darji <87583119+yshraj@users.noreply.github.com>
2022-08-28 16:29:15 +0530

    Create HeapSort.cpp

---
+++ Heap/HeapSort.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void heapify(int *arr,int n,int i)
+{
+    int largest = i;
+    int left = 2*i +1 ;
+    int right = 2*i + 2;
+    
+    if(left<n && arr[left] >arr[largest])
+    {
+        largest=left;
+    }
+    if(right<n && arr[right] >arr[largest])
+    {
+        largest=right;
+    }
+    
+    if(largest!=i)
+    {
+        swap(arr[i],arr[largest]);
+        heapify(arr,n,largest);
+    }
+    
+}
+void buildheap(int *arr,int n)
+{
+    for(int i = (n-2)/2 ; i>=0 ; i--) heapify(arr,n,i);
+}
+void heapsort(int *arr,int n)
+{
+    
+    buildheap(arr,n);
+    
+    for(int i = n-1;i>=1;i--)
+    {
+        swap(arr[0],arr[i]);
+        
+        heapify(arr,i,0);
+        
+    }
+}
+int main()
+{
+    int n;
+    cout<<"Size of Array:"<<endl;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++) cin>>arr[i];
+    
+    heapsort(arr,n);
+
+    for(int i=0;i<n;i++) cout<<arr[i]<<" ";
+    cout<<endl;
+}
